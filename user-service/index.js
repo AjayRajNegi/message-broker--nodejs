b43376e3a8ebc1eec93f4461658c662972870acc
Ajay <ajayrajnegi111@gmail.com>
2025-11-24 14:20:23 +0530

    Initial application setup and /users endpoint to save users in the database

---
+++ user-service/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+mongoose
+  .connect(`mongodb://localhost:27017/users`)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const user = new User({ name, email });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error saving:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
