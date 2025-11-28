be8fe9b7db26ac39f04cfebd1a13a147410ebd96
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-11-28 17:34:11 +0530

    notification service implemented with rabbitMQ

--- task-service/index.js
+++ task-service/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import amqp from "amqplib";
 
 const app = express();
 const port = 3002;
@@ -37,11 +38,40 @@ app.post("/tasks", async (req, res) => {
   }
 });
 
+let channel, connection;
+
+async function connectionRabbitMQWithRetry(retries = 5, delay = 3000) {
+  while (retries) {
+    try {
+      connection = await amqp.connect("amqp://rabbitmq_node");
+      channel = await connection.createChannel();
+      await channel.assertQueue("task_created");
+      console.log("Connected to RabbitMQ");
+      return;
+    } catch (error) {
+      console.error("RabbitMQ Connection Error:", error.message);
+      retries--;
+      console.log("Retrying again:", retries);
+      await new Promise((res) => setTimeout(res, delay));
+    }
+  }
+}
+
 app.get("/tasks", async (req, res) => {
   const tasks = await Task.find();
+
+  const message = { taskId: tasks._id, userId, title };
+
+  if (!channel) {
+    return res.status(503).json({ error: "RabbitMQ not connected." });
+  }
+
+  channel.sendToQueue("task_created", Buffer.from(JSON.stringify(message)));
+
   res.json(tasks);
 });
 
 app.listen(port, () => {
   console.log(`Task service listening on port ${port}`);
+  connectionRabbitMQWithRetry();
 });
